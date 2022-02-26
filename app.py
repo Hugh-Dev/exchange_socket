71443c3249d9ab92b80e04c13acc8e03716ada49
Hugh-Dev <hughpythoneer@gmail.com>
2022-02-26 15:57:13 -0800

    update

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 from flask import Flask, request, render_template, url_for
 from settings import PORT, DEBUG
-from socket_test import Serializers, Wsocket
+from main import Serializers, Render
 
 app = Flask(__name__)
 
+
+
 @app.route('/', methods=['GET'])
-def Trading(self):
+def index():
     if request.method == 'GET':
-
-        """ Websocket"""
-        ws = Wsocket()
-        on_open = ws.on_open()
-        on_send = ws.on_send()
-        data = ws.on_data()
-        on_date = ws.on_date()
-
-        """ Formatting"""
-        sls = Serializers()
-        bid, ask = sls.formatting(data=data)
-        date = sls.formatter_date(on_date)
-        spread = sls.calculate_spread(bid=bid, ask=ask)
-
-        return render_template('template.index.html', date=date)
+        
+        bid, ask, date = Render.test()
+        spread = bid - ask
+        spread = '{:4f}'.format(spread)
+        
+        if spread > '0':
+            action = 'Buy'
+            
+        elif spread < '0':
+            action = 'Sell'
+        
+        else:
+            action = 'Without change'
+        
+        return render_template('template.index.html', bid=bid, ask=ask, date=date, spread=spread, action=action)
 
     else:
         return render_template('template.400.html')
--- main.py
+++ main.py
@@ -35,26 +35,32 @@ class Serializers:
         self.date = date['date']
         return self.date
 
-
     def calculate_spread(self, bid, ask, date):
+
         self.spread = bid - ask
         self.spread = '{:4f}'.format(self.spread)
+        self.table = PrettyTable()
+
         if self.spread > '0':
-            table = PrettyTable()
-            table.field_names = ["date", "Bid price", "Ask price", "Spread", 'Action']
-            table.add_row([date, bid, ask, self.spread, 'Buy'])
-            print(table)
+            self.buy = 'Buy' 
+            self.table.field_names = ["Date", "Bid price", "Ask price", "Spread", 'Action']
+            self.table.add_row([date, bid, ask, self.spread, 'Buy'])
+            print(self.table)
+
         elif self.spread < '0':
-            table = PrettyTable()
-            table.field_names = ["date", "Bid price", "Ask price", "Spread", 'Action']
-            table.add_row([date, bid, ask, self.spread, 'Sell'])
-            print(table)
+            self.sell = 'Sell'
+            self.table.field_names = ["Date", "Bid price", "Ask price", "Spread", 'Action']
+            self.table.add_row([date, bid, ask, self.spread, 'Sell'])
+            print(self.table)
+
         else:
-            table = PrettyTable()
-            table.field_names = ["date", "Bid price", "Ask price", "Spread", 'Action']
-            table.add_row([date, bid, ask, self.spread, 'Without change'])
-            print(table)
+            self.without_change = 'Without change'
+            self.table.field_names = ["Date", "Bid price", "Ask price", "Spread", 'Action']
+            self.table.add_row([date, bid, ask, self.spread, 'Without change'])
+            print(self.table)
+
         return self.spread
+
 
 class Queue(Serializers):
 
@@ -82,16 +88,58 @@ class Queue(Serializers):
         return self.mean, self.count_ask, self.count_bid
 
 
-class Trading:
 
-    def sell(self):
-        pass
 
-    def buy(self):
-        pass
+
 
 if __name__ == "__main__":
+    
+    try:
+        """ Websocket"""
+        ws = Wsocket()
+        on_open = ws.on_open()
+        on_send = ws.on_send()
+        data = ws.on_data()
+        on_date = ws.on_date()
 
+        """ Formatting"""
+        sls = Serializers()
+        bid, ask = sls.formatting(data=data)
+        date = sls.formatter_date(on_date)
+        spread = sls.calculate_spread(bid=bid, ask=ask, date=date)
+
+        """Queue"""
+        q = Queue()
+        bid_long, ask_long = q.loop(data)
+        mean, count_ask, count_bid = q.calculate_long_spread()
+
+        """Table"""
+        table = PrettyTable()
+        table.field_names = ["date", "last", "Bid price", "Ask price", "Spread"]
+        table.add_row([date, 10, '{0:4f}'.format(count_bid), '{0:4f}'.format(count_ask), '{0:4f}'.format(mean)])
+        print(table)
+
+        """End"""
+        # ws.shutdown()
+        
+    except ValueError:
+        print("Oops!  Failed to open websocket...")
+
+    
+
+else:
+    """Render template"""
+    print('test...')
+    class Render():
+
+        def __init__(self, bid, ask, date):
+            self.bid = bid
+            self.ask = ask
+            self.date = date
+
+
+        def test():
+            return bid, ask, date
 
     """ Websocket"""
     ws = Wsocket()
@@ -104,23 +152,5 @@ if __name__ == "__main__":
     sls = Serializers()
     bid, ask = sls.formatting(data=data)
     date = sls.formatter_date(on_date)
-    spread = sls.calculate_spread(bid=bid, ask=ask, date=date)
 
-    """Queue"""
-    q = Queue()
-    bid_long, ask_long = q.loop(data)
-    mean, count_ask, count_bid = q.calculate_long_spread()
-
-    """Table"""
-    table = PrettyTable()
-    table.field_names = ["date", "last", "Bid price", "Ask price", "Spread"]
-    #table.add_row([1, '{0:4f}'.format(bid), '{0:4f}'.format(ask), '{0:4f}'.format(spread)])
-    table.add_row([date, 10, '{0:4f}'.format(count_bid), '{0:4f}'.format(count_ask), '{0:4f}'.format(mean)])
-    print(table)
-
-    """Render template"""
-
-
-    """End"""
-    # ws.shutdown()
-
+    Render(bid, ask, date)
